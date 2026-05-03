890ebace01d7ce14e47309017552c5441ac10691
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-03 20:46:14 +0600

    add nid answers

---
+++ Semester-1_Midterm/ques2/count2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+int main()
+{
+    char str[101];
+    scanf("%s", str);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+    int count1 = 0;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u')
+        {
+            count1++;
+        }
+    }
+
+    printf("%d", count1);
+
+    return 0;
+}
