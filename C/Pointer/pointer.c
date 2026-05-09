456e84b32a9954010cd9a715208cc202ffade956
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-09 23:35:07 +0600

    update

--- C/Pointer/pointer.c
+++ C/Pointer/pointer.c
@@ -11,8 +11,8 @@ int main()
     printf("%p\n", &ptr);
     printf("%d\n", *ptr); // access the value of ptr variable
 
+    // dereferencing - change value, print the change etc
     *ptr = 200;
-    // dereferencing
     printf("%d\n", x);
 
     return 0;
