5dbccd6183540e61a8fee8a5065865c3ac48f560
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-13 14:49:22 +0600

    reverse print using recursion

---
+++ C/Recursion/print/print_1toN.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void hello(int i)
+{
+    if (i == 6)
+    {
+        return;
+    }
+    hello(i + 1);
+    printf("%d\n",i);
+}
+int main()
+{
+    // printf("Hi\n");
+    int i = 1;
+    hello(i);
+    return 0;
+}
+
+
+// include <stdio.h>
+// void hello(int i)
+// {
+//     if (i == 6)
+//     {
+//         return;
+//     }
+//     printf("%d\n",i);
+//     hello(i + 1);
+// }
+// int main()
+// {
+//     // printf("Hi\n");
+//     int i = 1;
+//     hello(i);
+//     return 0;
+// }
