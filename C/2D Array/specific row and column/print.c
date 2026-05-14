da0779366534077fad038d0cd20214424f6881dc
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-15 05:12:37 +0600

    chech a matix is diagonal or not

---
+++ C/2D Array/specific row and column/print.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d ", &a[i][j]);
+        }
+    }
+
+    int row;
+    scanf("%d",&row);
+
+    for (int i = 0; i < c; i++)
+    {
+        printf("%d ",a[row][i]);
+    }
+
+
+    /*==========
+    SPECIFIC COLUMN PRINT
+    ===========*/
+
+
+    // int col;
+    // scanf("%d", &col);
+
+    // for (int i = 0; i < r; i++)
+    // {
+    //     printf("%d\n", a[i][col]);
+    // }
+
+    return 0;
+}
---
+++ C/2D Array/specific row and column/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+int row;
+    scanf("%d",&row);
+
+    for (int i = 0; i < c; i++)
+    {
+        printf("%d ",a[row][i]);
+    }
