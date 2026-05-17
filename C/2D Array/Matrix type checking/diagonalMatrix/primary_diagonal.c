aa74177e7d6e874c963b820d780f7678e3b08f0f
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-17 11:30:09 +0600

    secondary diagonal check

--- C/2D Array/Matrix type checking/diagonalMatrix/primary_diagonal.c
+++ C/2D Array/Matrix type checking/diagonalMatrix/primary_diagonal.c
@@ -1,103 +1,101 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main()
-{
-    int r, c;
-    scanf("%d %d", &r, &c);
-
-    if (r != c)
-    {
-        printf("Not a square matrix");
-        return 0;
-    }
-
-    int a[r][c];
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    bool is_diagonal = true;
-
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (i != j && a[i][j] != 0)
-            {
-                is_diagonal = false;
-                break;
-            }
-        }
-        if (!is_diagonal)
-            break;
-    }
-
-    if (is_diagonal)
-        printf("This is a primary diagonal matrix");
-    else
-        printf("This is not a primary diagonal matrix");
-
-    return 0;
-}
-
-
-
-
 // #include <stdio.h>
 // #include <stdbool.h>
 
 // int main()
 // {
-
 //     int r, c;
 //     scanf("%d %d", &r, &c);
+
+//     if (r != c)
+//     {
+//         printf("Not a square matrix");
+//         return 0;
+//     }
+
 //     int a[r][c];
 //     for (int i = 0; i < r; i++)
 //     {
 //         for (int j = 0; j < c; j++)
 //         {
-//             scanf("%d ", &a[i][j]);
+//             scanf("%d", &a[i][j]);
 //         }
 //     }
-
 
 //     bool is_diagonal = true;
-//     if (r == c)
+
+//     for (int i = 0; i < r; i++)
 //     {
-//         //
-//         for (int i = 0; i < r; i++)
+//         for (int j = 0; j < c; j++)
 //         {
-//             for (int j = 0; j < c; j++)
+//             if (i != j && a[i][j] != 0)
 //             {
-//                 if (i == j)
-//                 {
-//                     // inside diagonal
-//                     // printf("%d ", a[i][j]);
-//                 }
-//                 else
-//                 {
-//                     // outside diagonal
-//                     is_diagonal = false;
-//                     if (a[i][j] != 0)
-//                     {
-//                         printf("This is not a primary diagonal matrix");
-//                     }
-//                 }
+//                 is_diagonal = false;
+//                 break;
 //             }
 //         }
-//         if (is_diagonal == true)
-//         {
-//             printf("this is a primary diagonal matrix");
-//         }
+//         if (!is_diagonal)
+//             break;
 //     }
+
+//     if (is_diagonal)
+//         printf("This is a primary diagonal matrix");
 //     else
-//     {
-//         printf("Not a square matrix");
-//     }
+//         printf("This is not a primary diagonal matrix");
+
 //     return 0;
 // }
+
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d ", &a[i][j]);
+        }
+    }
+
+
+    bool is_diagonal = true;
+    if (r == c)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (i == j)
+                {
+                    // we are now at diagonal
+                }
+                else
+                {
+                    // outside diagonal
+                    if (a[i][j] != 0)
+                    {
+                        is_diagonal = false;
+                        printf("This is not a primary diagonal matrix");
+                    }
+                }
+            }
+        }
+        if (is_diagonal == true)
+        {
+            printf("this is a primary diagonal matrix");
+        }
+    }
+    else
+    {
+        printf("Not a square matrix");
+    }
+    return 0;
+}
---
+++ C/2D Array/Matrix type checking/diagonalMatrix/secondary_diagonal.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d ", &a[i][j]);
+        }
+    }
+
+
+    bool is_diagonal = true;
+    if (r == c)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (i + j==r-1)
+                {
+                    // we are now at secondary_diagonal
+                }
+                else
+                {
+                    // outside diagonal
+                    if (a[i][j] != 0)
+                    {
+                        is_diagonal = false;
+                        printf("This is not a secondary diagonal matrix\n");
+                        break;
+                    }
+                }
+            }
+        }
+        if (is_diagonal == true)
+        {
+            printf("this is a secondary diagonal matrix");
+        }
+    }
+    else
+    {
+        printf("Not a square matrix");
+    }
+    return 0;
+}
