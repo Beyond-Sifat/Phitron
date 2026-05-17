d73de9f8ced889073bd5c9a3d1bcfddd99e2c7de
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-17 19:31:49 +0600

    print last row and column of a 2d array

---
+++ C/FINAL/print last_row_column/matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d ", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", a[r - 1][j]);
+        }
+        break;
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < r; i++)
+    {
+        printf("%d ", a[i][c - 1]);
+    }
+    return 0;
+}
