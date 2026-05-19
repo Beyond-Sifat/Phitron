21b8566fb1abbcc6031f78bade1582b7554a39ff
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-19 23:58:10 +0600

    output in c++

---
+++ C++ For DSA/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Hello World" <<"\n";
+
+    int x = 10;
+    std::cout << x <<"\n";
+    char a = 'A';
+    double d = 12.12;
+std:: cout << a << std:: endl << d; //std:: endl -> new line
+    return 0;
+}
