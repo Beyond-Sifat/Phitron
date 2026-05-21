4f3b19c3712db1c7f2b756545d87bd556baf4662
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-21 19:01:49 +0600

    setprecision

---
+++ C++ For DSA/setprecision.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip> //input out manupulation
+using namespace std;
+int main(){
+    double c =38.232367;
+
+    cout << fixed << setprecision(3)<< c;
+    return 0;
+}
