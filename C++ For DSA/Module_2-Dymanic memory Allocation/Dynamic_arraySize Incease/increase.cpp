9c7e8ad2a78435febd3ef2d16b52f6fb884f73de
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-05-30 23:13:54 +0600

    update

--- C++ For DSA/Module_2-Dymanic memory Allocation/Dynamic_arraySize Incease/increase.cpp
+++ C++ For DSA/Module_2-Dymanic memory Allocation/Dynamic_arraySize Incease/increase.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    int *a = new int[5];
-    for (int i = 0; i < 5; i++)
+    int *a = new int[3];
+    for (int i = 0; i < 3; i++)
     {
         cin >> a[i];
     }
