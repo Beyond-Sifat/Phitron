7a5b5fe240d8066098a9e4c76b16a09098ecca1c
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-06-23 20:32:03 +0600

    string modifiers function

---
+++ C++ For DSA/String Class Obj/String Modifiers/str_modifiers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s ="HEllo World";
+    string s2="Hi";
+    // s+=s2;
+    // s.append(s2);
+    // s[0]='G';
+    // s.push_back('A');
+    // s.pop_back();
+    // s.assign("hello");
+    // s.erase(2,5);
+    cout<<s<<endl;
+    return 0;
+}
