64e9eea27d4933c2fd456d2f1a2629ee52df3499
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-06-26 03:13:14 +0600

    feat(cpp):add range based for loop using char and int

---
+++ C++ For DSA/module-6 more about string/Range Based for loop/range_loop.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s;
+    cin >> s;
+
+    // for(int i=0;i<s.size();i++){
+    //     cout<<s[i]<<endl;
+    // }
+    for (char c : s)
+    {
+
+        cout << c << endl;
+    }
+
+
+    int a[6]={1 ,2, 3, 4 ,5 ,6};
+    for(int i:a){
+        cout << i << endl;
+    }
+
+    // cout<<s;
+    return 0;
+}
