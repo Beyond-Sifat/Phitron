b20ea0eb8c37399b2787cab9f86e5d544739400d
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-06-26 01:08:23 +0600

    feat(cpp): add string with space and example

---
+++ C++ For DSA/String Class Obj/String Input With Space/space.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    string s;
+    cin.ignore();
+    getline(cin, s);
+    cout << x << endl;
+    cout << s << endl;
+
+    // char str[101];
+    // cin.getline(str,100);
+    // cout<<str<<endl;
+    return 0;
+}
