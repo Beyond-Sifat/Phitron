d2dd462692fa93aa43198c4198177688c40e5d80
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-07-04 21:10:31 +0600

    add questions

--- C++ For DSA/Final/Find and replace word/replace.cpp
+++ C++ For DSA/Final/Find and replace word/replace.cpp
@@ -1,3 +1,28 @@
+// *Problem Statement
+
+// *You will be given two strings S and X. You need to replace all X from string S with a '#' sign.
+
+// First line will contain T, the number of test cases.
+// Next T lines will contain a line with S and X.
+// Constraints
+
+// 1 <= T <= 1000
+// 1 <= |S|, |X| <= 1000
+// |X| <= |S|
+// Output Format
+
+// For each test cases output the modified string S.
+
+// *Sample Input 0
+// 2
+// rahimisagoodguy good
+// canyoutellmewhereicanfindheriwillbegreatefultoyouifyoutellmetheanswer you
+
+//* Sample Output 0
+// rahimisa#guy
+// can#tellmewhereicanfindheriwillbegreatefulto#if#tellmetheanswer
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
