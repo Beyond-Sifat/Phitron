14a33874b2728f3fafdc98b0dcd122b97b7ac102
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-07-19 21:52:03 +0600

    update

--- DSA/Module-2_STL Vector/Vector-Capacity function/vector_capacity.cpp
+++ DSA/Module-2_STL Vector/Vector-Capacity function/vector_capacity.cpp
@@ -4,6 +4,7 @@ int main()
 {
 
     vector<int> v = {1, 2, 3};
+
     v.push_back(7);
     cout << v.size() << endl;
     v.clear();
@@ -14,13 +15,15 @@ int main()
     vector<int> v2;
     cout << v2.empty() << endl;
 
-    // resize function
+    // resize function and capacity
 
     vector<int> v3;
+     cout << v3.capacity() << endl;
     v3.push_back(10);
     cout << v3.size() << endl;
     v3.resize(2);
     cout << v3.size() << endl;
+    cout << v3.capacity() << endl;
 
     return 0;
 }
