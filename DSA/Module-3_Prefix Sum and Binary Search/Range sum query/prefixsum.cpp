86df813b5dfab65409d5f7042e11f26ba0ec646c
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-07-27 23:08:30 +0600

    optimize range sum query using prefix sum

---
+++ DSA/Module-3_Prefix Sum and Binary Search/Range sum query/prefixsum.cpp
@@ -0,0 +1,46 @@
+
+// ****https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> pre(n + 1);
+    pre[1] = a[1];
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + a[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << pre[i] << " ";
+    }
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        int sum = 0;
+        if (l == 1)
+        {
+            sum = pre[r];
+        }
+        else
+        {
+            sum = pre[r] - pre[l - 1];
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
