cd959605f7045df95309286385018de9748757e6
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-08-01 23:14:31 +0600

    ckeck duplicate

---
+++ DSA/Module-4 Assignment-1/isDuplicate/duplicate.cpp
@@ -0,0 +1,56 @@
+//*You will be given an array A of size N. Print "YES" if there is any duplicate value in the array, "NO" otherwise.
+
+// 1 <= N <= 100000
+// 0 <= A[i] <= 10^9; Where 0 <= i < N
+// Output Format
+
+// Output "YES" or "NO" without the quotation marks according to the problem statement.
+//* Sample Input 0
+// 5
+// 1 2 3 4 5
+// *Sample Output 0
+// NO
+
+// *Sample Input 1
+// 6
+// 2 1 3 5 2 1
+// *Sample Output 1
+// YES
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    bool duplicate = false;
+    sort(a.begin(), a.end());
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] == a[i - 1])
+        {
+            duplicate = true;
+            break;
+        }
+    }
+    if (duplicate)
+    {
+
+        cout << "YES";
+    }
+    else
+    {
+
+        cout << "NO";
+    }
+
+    return 0;
+}
