f10fd5e6a1eb5ece6fa7a1aed9b74df0d5506f52
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-08-13 11:04:48 +0600

    take input from user

---
+++ DSA/Module-7 More Singly Linked List/input a linked list/linkedList_input.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node* &tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail=newNode;
+        return;
+    }
+
+   tail->next = newNode;
+   tail = newNode;
+}
+void printing_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+    printing_linked_list(head);
+
+    return 0;
+}
