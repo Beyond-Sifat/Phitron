7297f143136dee81166f2efad4d18c7822ea4e46
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-08-17 19:44:11 +0600

    colve coller problem

---
+++ DSA/Module-7 More Singly Linked List/Special Problem Codechef/Cooler.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, M;
+        cin >> N >> M;
+        
+    }
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int T;
+//     cin >> T;
+
+//     while (T--)
+//     {
+//         int N, M;
+//         cin >> N >> M;
+//         int total =0;
+//         for(int i=N;i>M;i--){
+//             total+=i;
+//         }
+//         cout<<total<<endl;
+//     }
+//     return 0;
+// }
