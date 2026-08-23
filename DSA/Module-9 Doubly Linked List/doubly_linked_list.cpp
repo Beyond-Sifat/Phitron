2ec3151c2e6e3a5f581d9ba3e62c90e9cb94c027
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-08-23 11:17:26 +0600

    doubly linked list create and forward, backward print

---
+++ DSA/Module-9 Doubly Linked List/doubly_linked_list.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void printing_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void printing_backward(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    printing_forward(head);
+    printing_backward(tail);
+    return 0;
+}
