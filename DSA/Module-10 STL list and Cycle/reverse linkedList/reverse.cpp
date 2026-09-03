86daded1600bb51d391846e834302eb2998b23e6
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-09-03 23:07:11 +0600

    reverse a linked list

---
+++ DSA/Module-10 STL list and Cycle/reverse linkedList/reverse.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+void printing_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+void reverse_linked_list(Node *&head, Node*&tail, Node *temp)
+{
+    // base case
+    if (temp->next == NULL)
+    {
+        head = temp;
+        return;
+    }
+    reverse_linked_list(head,tail, temp->next);
+    temp->next->next = temp;
+    temp->next = NULL;
+    tail=temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+    reverse_linked_list(head, tail,head);
+    printing_linked_list(head);
+
+    return 0;
+}
