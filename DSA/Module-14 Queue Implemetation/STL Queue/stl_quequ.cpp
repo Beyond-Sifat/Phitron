766f8db77a8b8bfddd1d8d0b550d50ab983ddc3a
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-09-12 22:17:41 +0600

    stl queue

---
+++ DSA/Module-14 Queue Implemetation/STL Queue/stl_quequ.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    queue<int> q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+        q.push(value);
+    }
+    while (q.empty() == false)
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    return 0;
+}
