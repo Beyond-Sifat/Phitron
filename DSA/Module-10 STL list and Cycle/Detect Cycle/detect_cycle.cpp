aac60aea42d42b06d011a153f2af1a0dd1cef5f5
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-09-15 23:09:36 +0600

    detect cycle in a linked list

---
+++ DSA/Module-10 STL list and Cycle/Detect Cycle/detect_cycle.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    // d->next = a;
+
+    Node *slow = head;
+    Node *fast = head;
+    bool flag=false;
+
+    while(fast!=NULL&&fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            flag=true;
+            //cycle detected
+            break;
+        }
+    }
+
+    if(flag==true){
+        cout<<"Cycle detected";
+    }else{
+        cout<<"Not detect";
+    }
+    return 0;
+}
