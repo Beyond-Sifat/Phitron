d306972bf8930f2137ebe692cae0420b38150b47
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-09-18 22:51:48 +0600

    count nodes in binary tree

---
+++ DSA/Module-18 Binary Tree Operations/Count Node in Binary Tree/count_node.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    if (val == -1)
+    {
+        return NULL;
+    }
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *parent = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+        parent->left = myLeft;
+        parent->right = myRight;
+
+        if (parent->left != NULL)
+        {
+            q.push(parent->left);
+        }
+
+        if (parent->right != NULL)
+        {
+            q.push(parent->right);
+        }
+    }
+    return root;
+}
+
+int count_node(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = count_node(root->left);
+    int r = count_node(root->right);
+    return l+r+1;
+}
+int main()
+{
+    Node *root = input_tree();
+    cout<<count_node(root);
+    return 0;
+}
---
+++ DSA/Module-18 Binary Tree Operations/Count Node in Binary Tree/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+int count_node(Node *root)
