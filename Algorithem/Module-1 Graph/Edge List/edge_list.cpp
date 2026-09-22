c97e3af2352a07671b11442bb673785f5b5838e0
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-09-22 19:22:48 +0600

    edge list

---
+++ Algorithem/Module-1 Graph/Edge List/edge_list.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int node, edge;
+    cin >> node >> edge;
+
+    vector<pair<int, int>> edge_list;
+    while (edge--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        edge_list.push_back({a, b});
+    }
+
+    for (pair<int, int> p : edge_list)
+    {
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+    return 0;
+}
